72db40f6ab484727dc7372ab3e5f2664da326662
devussy <seungyong.yun@styleshare.kr>
2021-05-29 18:07:50 +0900

    + 사용자의 권한 거부에 대응하기

--- app/src/main/java/fastcampus/aop/part2/chapter07/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/chapter07/MainActivity.kt
@@ -1,12 +1,17 @@
 package fastcampus.aop.part2.chapter07
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -57,7 +62,11 @@ class MainActivity : AppCompatActivity() {
                 grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
 
         if (!audioRecordPermissionGranted) {
-            finish()
+            if (!shouldShowRequestPermissionRationale(permissions.first())) {
+                showPermissionExplanationDialog()
+            } else {
+                finish()
+            }
         }
     }
 
@@ -149,6 +158,21 @@ class MainActivity : AppCompatActivity() {
         soundVisualizerView.stopVisualizing()
         recordTimeTextView.stopCountUp()
         state = State.AFTER_RECORDING
+    }
+
+    private fun showPermissionExplanationDialog() {
+        AlertDialog.Builder(this)
+            .setMessage("녹음 권한을 켜주셔야지 앱을 정상적으로 사용할 수 있습니다. 앱 설정 화면으로 진입하셔서 권한을 켜주세요.")
+            .setPositiveButton("권한 변경하러 가기") { _, _ -> navigateToAppSetting() }
+            .setNegativeButton("앱 종료하기") { _, _ -> finish() }
+            .show()
+    }
+
+    private fun navigateToAppSetting() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri: Uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
     }
 
     companion object {
